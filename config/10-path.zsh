c5bd0b0bebac13c17d5b7722fa068a41c3c5ae1f
Julien Ridoux <julien@synclab.org>
2012-07-30 18:09:25 +1000

    Update path definition with Lion and macports stuff.

--- config/10-path.zsh
+++ config/10-path.zsh
@@ -2,34 +2,39 @@
 # PATH
 # ------------------------------------------------------------------------------
 
-# Should be sourced early on since other configs may rely on it
+# Should be sourced early on since other configs may rely on it.
+# Give priority to exotic locations over the default ones.
+#PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/pkg/bin
 
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/pkg/bin
+PATH=''
 
 if [[ -d "$HOME/bin" ]]; then
 	# Push my bin dir first to override stupid Apple broken utils
-	PATH=$HOME/bin:$PATH
+	PATH+=$HOME/bin
 fi
 
 if [[ -d "/sw/bin" ]]; then
 	PATH+=:/sw/bin
 fi
 
-if [[ -d "/usr/local/teTeX/bin/powerpc-apple-darwin-current/" ]]; then
-	PATH+=:/usr/local/teTeX/bin/powerpc-apple-darwin-current
-fi
 if [[ -d "/usr/texbin/" ]]; then
 	PATH+=:/usr/texbin
 fi
+
 if [[ -d "/usr/local/texbin/" ]]; then
 	PATH+=:/usr/local/texbin
 fi
+
+if [[ -d "/opt/local/libexec/gnubin/" ]]; then
+	PATH+=:/opt/local/libexec/gnubin
+fi
+
 if [[ -d "/opt/local/bin/" ]]; then
 	PATH+=:/opt/local/bin
 fi
 
 if [[ -d "/usr/lib/distcc" ]]; then
-	PATH=/usr/lib/distcc:${PATH}
+	PATH+=:/usr/lib/distcc
 fi
 
 # GIT completion
@@ -40,5 +45,28 @@ fi
 # GIT binaries
 if [[ -d "/usr/local/git/bin" ]]; then
 	PATH+=:/usr/local/git/bin
+fi
+
+
+# Standard paths at the end
+
+if [[ -d '/bin' ]]; then 
+	PATH+=:/bin
+fi
+
+if [[ -d '/sbin' ]]; then 
+	PATH+=:/sbin
+fi
+
+if [[ -d '/usr/bin' ]]; then 
+	PATH+=:/usr/bin
+fi
+
+if [[ -d '/usr/local/bin' ]]; then 
+	PATH+=:/usr/local/bin
+fi
+
+if [[ -d '/usr/local/sbin' ]]; then 
+	PATH+=:/usr/local/sbin
 fi
 
