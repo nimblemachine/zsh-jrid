7f9f2d12ead7b84a41695b38288c6cbc853647c9
Julien Ridoux <julien@synclab.org>
2014-03-14 11:04:51 +1100

    Extra ls coloring for cpp and ino files

--- config/50-ls-colors.zsh
+++ config/50-ls-colors.zsh
@@ -62,7 +62,7 @@ SOUND_COLORS='*.mp3=00;35:*.mod=00;35:*.wav=00;35:*.mid=00;35:*.xm=00;35:*.s3m=0
 MOVIE_COLORS='*.mpg=00;35:*.avi=00;35:*.mov=00;35'
 ARCHIVE_COLORS='*.tar=00;31:*.tgz=00;31:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.zip=00;31:*.bz2=00;31:*.rpm=00;31:*.deb=00;31:*.z=00;31:*.Z=00;31:*.gz=00;31'
 EXE_COLORS='*.cmd=00;32:*.exe=00;32:*.com=00;32:*.btm=00;32:*.bat=00;32'
-CODE_COLORS='*.c=00;33:*.h=00;95:*.gl=00;33:*.cc=00;33:*.m=00;33:*.php=00;33:*.tex=00;33'
+CODE_COLORS='*.c=00;33:*.h=00;95:*.gl=00;33:*.cc=00;33:*.cpp=00;33:*.ino=00;33:*.m=00;33:*.php=00;33:*.tex=00;33'
 
 # Understood by GNU ls and used for completion
 LS_COLORS="no=00:fi=00:di=00;36:ln=00;35:pi=47;33:so=00;35:bd=47;33;00:cd=47;33;00:or=47;31;00:ex=00;32:${IMG_COLORS}:${SOUND_COLORS}:${MOVIE_COLORS}:${ARCHIVE_COLORS}:${EXE_COLORS}:${CODE_COLORS}";
